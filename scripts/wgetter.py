be68e12f20fac9865865bdaa83b43de6c01998dc
Albrigs <natanfernandessantos@protonmail.com>
2020-09-20 00:14:01 -0300

    Busca index dos ignores no github e recuperação de seus textos

---
+++ scripts/wgetter.py
@@ -0,0 +1,28 @@
+from pprint import pprint
+from requests import get
+from base64 import b64decode
+
+b_url_ignore = "https://api.github.com/repos/github/gitignore/"
+
+def git_indexes():
+    """
+    Pega os nomes e url dos ignores principais do git
+    """
+    # TODO: Adicionar a busca dentro das pastas do diretório
+
+    tmp_ignore_data = get(b_url_ignore+"git/trees/master").json()['tree']
+
+    ignore_paths = []
+    for e in tmp_ignore_data:
+        if e["path"].endswith("ignore"): ignore_paths.append(e["path"])
+
+    ignore_data_from_git = {}
+    for e in ignore_paths:
+        ignore_data_from_git[e.split('.')[0].lower()] = b_url_ignore+"contents/"+e
+
+    return ignore_data_from_git
+
+def get_ignore(target_url):
+    """Busca um ignore no repositório e retorna seu texto"""
+    url = get(target_url).json()["git_url"]
+    return b64decode(get(url).json()["content"])
